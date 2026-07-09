4c3c087fb153ecfa59f09230dbf04c281d4b6688
hmatei <hannamatei97@gmail.com>
2026-07-09 12:26:54 -0700

    Add files via upload
    Committing my function assignment

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function displayMessage() {
+    var greeting = "Welcome to my Javascript function project!";
+    var ending = "Thanks for clicking the button!";
+    document.getElementById("Message").innerHTML = greeting + " " + ending;
+}
