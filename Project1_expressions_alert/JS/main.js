c765b403b621d8dc988d86f4263125b71438f25f
hmatei <hannamatei97@gmail.com>
2026-07-09 12:01:47 -0700

    Add files via upload
    Committing my "BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT"

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("Welcome to my JavaScript project!"); //Display an alert box.
+document.write("Hello, World!<br><br>"); //Display greeting message on the webpage.
+var Sent1 = "This is the beginning of the string"; //Creates the first string variable.
+var Sent2 = " and this is the end of the string."; //Creates the second string variable.
+document.write(Sent1 + Sent2); //Concatenate the two string variables together and display the result on the webpage.
+document.write("<br><br>"); //Add line breaks for better readability.
+document.write(30 + 3 ); //Writting an expression and displaying the result on the webpage.
+document.write("<br><br>"); //Add line breaks for better readability.
+var name = "Hanna"; //Creates another variable
+document.write("Created by " + name); //Concatenate the string with the variable and display it on the webpage.
