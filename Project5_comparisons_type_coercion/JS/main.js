1657440a5758151ae186ed5001994c028a325d53
hmatei <hannamatei97@gmail.com>
2026-07-09 20:17:16 -0700

    Add files via upload
    Committing 'PROJECT5_COMPARISONS_TYPE_COERCION SUBMISSION ASSIGNMENT"

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,11 @@
+var city = "Portland"; 
+document.write("The data type of city is: " + typeof city + "<br>"); //Display the data type of a variable
+document.write("My favorite number is " + 7 + "<br>"); //Combine a string anf a number
+document.write("25 == '25': " + (25 == "25") + "<br>"); //== Compares only the values
+document.write("25 === '25': " + (25 === "25") + "<br>"); //=== Compares both the values and the data types
+document.write("50 > 35: " + (50 > 35) + "<br>"); //Greater than operator
+document.write("18 < 10: " + (18 < 10) + "<br>"); //Less than operator
+document.write("8 < 10 && 12 > 5): " + (8 < 10 && 12 > 5) + "<br>"); //&& is the AND operator
+document.write("3 < 9 || 15 == 15): " + (3 < 9 || 15 == 15) + "<br>"); //|| is the OR operator
+document.write("!(9 < 5): " + !(9 < 5) + "<br>"); //! is the NOT operator
+document.write("!(6 > 2): " + !(6 > 2) + "<br>"); //NOT operator
