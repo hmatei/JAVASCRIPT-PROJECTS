5eb26ee14abf4cfe1385d6001b48a3de3d235cfb
hmatei <hannamatei97@gmail.com>
2026-07-26 23:09:21 -0700

    Committing One-Page Travel Website
    ONE-PAGE WEBSITE SUBMISSION ASSIGNMENT

---
+++ One-Page Website/JS/one_page_website.js
@@ -0,0 +1,34 @@
+//Lightbox will be added
+let slideIndex = 1;
+function openModal() {
+    document.getElementById("myModal").style.display = "block"
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none"
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
